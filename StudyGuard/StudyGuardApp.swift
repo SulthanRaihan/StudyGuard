870bf5843d83c79cf8a6d153887049a1808fb2b5
Sunraku <sulthanraihan701@gmail.com>
2026-06-12 19:34:55 +0800

    Initial Commit

---
+++ StudyGuard/StudyGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyGuardApp.swift
+//  StudyGuard
+//
+//  Created by 20 on 2026/6/12.
+//
+
+import SwiftUI
+
+@main
+struct StudyGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
